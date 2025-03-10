f3b1385244c8f5242c2f6135efeda20cb4289f30
Valeria Pacheco <vpachecosanmartin@gmail.com>
2025-03-10 18:33:28 -0500

    Organized the header and created a hamburger menu

--- src/components/Layouts/Header/Header.jsx
+++ src/components/Layouts/Header/Header.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { NavLink } from 'react-router-dom'
 import "./StylesHeader.css"
 import "../../../index.css"
 export const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <header className='containerHeader'>
-        <nav className='navHeader'>
-            <ul className='containerItemsHeader'>
-                <li className='containerLiHeader'><NavLink to="/">Inicio</NavLink></li>
-                <li className='containerLiHeader'><NavLink to="/skills">Habilidades</NavLink></li>
-                <li className='containerLiHeader'><NavLink to="/socialskills">Habilidades Sociales</NavLink></li>
-                <li className='containerLiHeader'><NavLink to="/studies">Estudios</NavLink></li>
-                <li className='containerLiHeader'><NavLink to="/experiences">Experiencia</NavLink></li>
-            </ul>
-        </nav>
+
+    <header className="containerHeader">
+      <div className="logo">Mi Logo</div>
+      {/* Botón de menú hamburguesa */}
+      <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </button>
+      {/* Menú de navegación */}
+      <nav className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <NavLink to="/">Inicio</NavLink>
+        <NavLink to="/skills">Habilidades</NavLink>
+        <NavLink to="/socialSkills">Habilidades Sociales</NavLink>
+        <NavLink to="/studies">Estudios</NavLink>
+        <NavLink to="/experiences">Experiencia</NavLink>
+      </nav>
     </header>
   )
 }

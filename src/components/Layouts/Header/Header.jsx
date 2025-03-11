33256749a11d783377f209393cea6a7d7a680b90
Valeria Pacheco <vpachecosanmartin@gmail.com>
2025-03-11 06:51:02 -0500

    Created the page Skills with Styles

--- src/components/Layouts/Header/Header.jsx
+++ src/components/Layouts/Header/Header.jsx
@@ -17,11 +17,11 @@ export const Header = () => {
       </button>
       {/* Menú de navegación */}
       <nav className={`nav-links ${menuOpen ? "open" : ""}`}>
-        <NavLink to="/">Inicio</NavLink>
-        <NavLink to="/skills">Habilidades</NavLink>
-        <NavLink to="/socialSkills">Habilidades Sociales</NavLink>
-        <NavLink to="/studies">Estudios</NavLink>
-        <NavLink to="/experiences">Experiencia</NavLink>
+        <NavLink to="/" onClick={() => setMenuOpen(false)}>Inicio</NavLink>
+        <NavLink to="/skills" onClick={() => setMenuOpen(false)}>Habilidades</NavLink>
+        <NavLink to="/socialSkills" onClick={() => setMenuOpen(false)}>Habilidades Sociales</NavLink>
+        <NavLink to="/studies" onClick={() => setMenuOpen(false)}>Estudios</NavLink>
+        <NavLink to="/experiences" onClick={() => setMenuOpen(false)}>Experiencia</NavLink>
       </nav>
     </header>
   )
--- src/components/Pages/Skills/Skills.jsx
+++ src/components/Pages/Skills/Skills.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { FaJava } from "react-icons/fa";
+import './Skills.css'
 
 export const Skills = () => {
   return (
     <div>
       <h2>Skills</h2>
+      
+      <li>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <FaJava className="icon"/>- Html
+        </a>
+      </li>
+      <li>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <FaJava className="icon"/>- Css
+        </a>
+      </li>
+      <li>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <FaJava className="icon"/>- React
+        </a>
+      </li>
+      <li>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <FaJava className="icon"/>- Java
+        </a>
+      </li>
+      <li>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <FaJava className="icon"/>- JavaScript
+        </a>
+      </li>
+      <li>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <FaJava className="icon"/>- Mongo
+        </a>
+      </li>
+      <li>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <FaJava className="icon"/>- MySqlWorbeach
+        </a>
+      </li>
     </div>
   )
 }
